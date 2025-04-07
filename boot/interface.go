2e8d0c4390270fb93f9cb674a22ce77c4834c689
hoangminhphuc <22028059@vnu.edu.vn>
2025-04-07 08:36:09 +0700

    Seperate logic for init and runtime service

--- boot/interface.go
+++ boot/interface.go
@@ -1,11 +1,40 @@
 package boot
 
-type Plugin func(*ServiceHub)
+import "github.com/hoangminhphuc/goph-chat/common/logger"
 
-type InitService interface {
+
+// Plug service in and play, unplug is easy
+type Plugin func(*serviceHub)
+
+type Service interface {
 	Name() string
 	InitFlags()
 	Run() error
-	Get() interface{}
 	Stop() <-chan error
+}
+
+// Services that need to be initialized before application runs
+type InitService interface {
+	Service
+	Get() interface{}
+}
+
+// Services that run with the application runtime
+type ActiveService interface {
+	Service
+}
+
+
+// A service hub that handles all services
+type ServiceHub interface {
+	GetName() string
+	GetLogger() logger.ZapLogger
+	initFlags()
+	parseFlags()
+	Init() error
+	Start() error
+	Run() <-chan error
+	Stop() error
+	GetService(name string) (interface{}, bool)
+	MustGetService(name string) interface{}
 }
