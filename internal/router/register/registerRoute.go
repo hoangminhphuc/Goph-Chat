c3833b9d03916c0babaa1bc3309afe0ce9e4e921
hoangminhphuc <22028059@vnu.edu.vn>
2025-04-10 22:49:37 +0700

    Add route for websocket

--- internal/router/register/registerRoute.go
+++ internal/router/register/registerRoute.go
@@ -3,12 +3,14 @@ package register
 import (
 	"github.com/gin-gonic/gin"
 	serviceHub "github.com/hoangminhphuc/goph-chat/boot"
+	roomWebSocketRoutes "github.com/hoangminhphuc/goph-chat/module/room/routes"
 	userRoutes "github.com/hoangminhphuc/goph-chat/module/user/routes"
 )
 
 func RegisterAllRoutes(router *gin.RouterGroup, serviceCtx serviceHub.ServiceHub) {
 	v1 := router.Group("/v1")
 
-	userRoutes.RegisterRoute(v1.Group("/users"), serviceCtx)
+	userRoutes.RegisterUserRoute(v1.Group("/users"), serviceCtx)
+	roomWebSocketRoutes.RegisterWebSocketRoute(v1.Group("/rooms"), serviceCtx)
 }
 
