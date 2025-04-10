ee6d3c1be92e7ca5d4f8da9428c80981a945a8e5
hoangminhphuc <22028059@vnu.edu.vn>
2025-04-10 22:51:36 +0700

    Add Read method for client to read and broadcast messages to pool

--- module/room/websocket/client.go
+++ module/room/websocket/client.go
@@ -1,18 +1,56 @@
 package websocket
 
-import "github.com/gorilla/websocket"
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/gorilla/websocket"
+)
+
+
+type Message struct {
+	RoomID 			int
+	ChatUser   string `json:"chatUser,omitempty"`
+}
 
 
 type Client struct {
 	ID         string
 	Connection *websocket.Conn
-	Send chan []byte
+	Pool   *Pool
 }
 
-func NewClient(id string, conn *websocket.Conn) *Client{
-	return &Client {
-		ID: id,
+func NewClient(pool *Pool, conn *websocket.Conn) *Client{
+	return &Client{
 		Connection: conn,
-		Send: make(chan []byte),
+		Pool:       pool,
+	}
+}
+
+func (c *Client) Read(bodyChan chan []byte) {
+	defer func() {
+		c.Pool.Unregister <- c
+		c.Connection.Close()
+	}()
+
+	for {
+		_, p, err := c.Connection.ReadMessage()
+		if err != nil {
+			log.Fatal(err)
+		}
+		var msg Message
+		err = json.Unmarshal(p, &msg)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Adds the sender's identity (email) to the message so others know who sent it.
+		message := Message {
+			RoomID: msg.RoomID,
+			ChatUser: c.ID,
+		}
+
+		// Sends the message to the Pool
+		c.Pool.Broadcast <- message
+		log.Println("info:", "Message received: ", message)
 	}
 }
