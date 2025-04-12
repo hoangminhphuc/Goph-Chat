4ed7584c9cfddeb6954d7ded92543bd5b40c648c
hoangminhphuc <22028059@vnu.edu.vn>
2025-04-12 15:38:10 +0700

    Fix minor issues with routing logic

--- module/room/routes/routes.go
+++ module/room/routes/routes.go
@@ -1,53 +1,16 @@
 package routes
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
 	serviceHub "github.com/hoangminhphuc/goph-chat/boot"
 	ws "github.com/hoangminhphuc/goph-chat/module/room/websocket"
 )
 
-var upgrader = websocket.Upgrader {
-	CheckOrigin: func(r *http.Request) bool { return true },
-}
-
 func RegisterWebSocketRoute(v1 *gin.RouterGroup, serviceCtx serviceHub.ServiceHub) {
-	pool := ws.NewPool()
-	go pool.Start()
+	roomManager := ws.GetRoomCenter()
 	chat := v1.Group("/ws")
 
 	chat.GET("/:id", func(c *gin.Context) {
-		ws.ServerWebSocket(c, pool)
-
-
-		// go func() {
-		// 	defer func() {
-		// 		pool.Remove(client.ID)
-		// 		client.Connection.Close()
-		// 	}()
-		// 	for {
-		// 		// Read message from the client (frontend)
-		// 		_, msg, err := client.Connection.ReadMessage()
-		// 		log.Printf("📥 Received from client %d: %s", client.ID, string(msg))
-		// 		if err != nil {
-		// 			break
-		// 		}
-		// 	// Broadcast to all send channel of clients so it can sends to the frontend
-		// 		pool.Broadcast(client.ID, msg)
-		// 	}
-		// }()
-
-		// go func() {
-		// 	for msg := range client.Send {
-		// 		// Send message to the client (frontend) if receive message
-		// 		if err := client.Connection.WriteMessage(websocket.TextMessage, msg); err != nil {
-		// 			break
-		// 		}
-		// 		log.Printf("📤 Sent to client %d: %s", client.ID, string(msg))
-		// 	}
-		// }()
-
+		ws.ServerWebSocket(c, roomManager)
 	})
 }
