4e6b0e8b5fa88cd6deaf6ea31aeecc100e9da13d
hoangminhphuc <22028059@vnu.edu.vn>
2025-04-15 16:17:25 +0700

    Add constants for security utils

--- common/consts.go
+++ common/consts.go
@@ -1,9 +1,22 @@
 package common
 
 /* 
-	! Constants for plugin prefix name
+	! Plugin prefix name consts
 */
 
 const (
 	PluginDBMain = "mysql"
+)
+
+/* 
+	! Security consts
+*/
+
+const (
+	// 32 bytes
+	DefaultSaltLength = 32
+	// Absolute maximum salt length to prevent resource exhaustion
+	MaxSaltLength = 1024
+
+	BCRYPT_COST = 12
 )
---
+++ common/utils/random.go
@@ -0,0 +1,52 @@
+package utils
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"log"
+
+	"github.com/hoangminhphuc/goph-chat/common"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func GenerateSalt(length int) (string, error) {
+
+	switch {
+	case length <= 0:
+		length = common.DefaultSaltLength
+	case length > common.MaxSaltLength:
+		length = common.MaxSaltLength
+	}
+
+	saltBytes := make([]byte, length)
+	// fills the slice with random unpredictable bytes by crypto/rand
+	if _, err := rand.Read(saltBytes); err != nil {
+		return "", err
+	}
+
+	// Encode using URL-safe base64 without padding
+	return base64.URLEncoding.WithPadding(base64.NoPadding).EncodeToString(saltBytes), nil
+}
+
+type BcryptHash struct{}
+
+func NewBcryptHash() *BcryptHash {
+	return &BcryptHash{}
+}
+
+func (*BcryptHash) Hash(plainPassword string) string {
+	// Set cost = 12. Higher cost = more secure, but slower.
+
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(plainPassword), common.BCRYPT_COST)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+
+	return string(hashedBytes)
+}
+
+func (*BcryptHash) Compare(hashedValue, plainText string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedValue), []byte(plainText))
+	return err == nil
+}
