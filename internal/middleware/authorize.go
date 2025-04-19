82b1051e0cff53fe5fbd92ad249da31923d66e93
hoangminhphuc <22028059@vnu.edu.vn>
2025-04-19 11:32:56 +0700

    Implement current user context logic per request/response

--- internal/middleware/authorize.go
+++ internal/middleware/authorize.go
@@ -2,11 +2,13 @@ package middleware
 
 import (
 	"context"
+	"log"
 	"net/http"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/hoangminhphuc/goph-chat/common"
+	"github.com/hoangminhphuc/goph-chat/common/models"
 	"github.com/hoangminhphuc/goph-chat/common/utils"
 	"github.com/hoangminhphuc/goph-chat/module/user/model"
 )
@@ -31,18 +33,31 @@ func RequireAuth(authenStore authenStore, secret string) func(*gin.Context) {
 		token, err := extractTokenFromHeaderString(c.GetHeader("Authorization"))
 
 		if err != nil {
-			c.JSON(http.StatusUnauthorized, err.Error())
-			c.AbortWithStatus(http.StatusUnauthorized)
+			common.ErrorResponse(c, http.StatusInternalServerError, err.Error())
 			return
 		}
 
-		_, err = utils.VerifyToken(token, secret)
+		payload, err := utils.VerifyToken(token, secret)
 
 		if err != nil {
-			c.JSON(http.StatusUnauthorized, err.Error())
-			c.AbortWithStatus(http.StatusUnauthorized)
+			common.ErrorResponse(c, http.StatusUnauthorized, err.Error())
 			return
 		}
+
+		user, err := authenStore.FindUser(c.Request.Context(), map[string]interface{}{"id": payload.UserId()})
+		
+		if err != nil {
+			common.ErrorResponse(c, http.StatusUnauthorized, err.Error())
+			return
+		}
+
+		currentUser := models.NewRequester(user.ID, user.Email, user.Role.String())
+
+		// Set throughout this whole request reponse route (each time access an URL)
+		c.Set(common.CurrentUser, currentUser)
+
+		keys := c.Keys
+		log.Println(keys) // Check if common.CurrentUser is present in the keys
 
 		c.Next()
 	}
