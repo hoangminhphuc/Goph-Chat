5ded7d4e0fc38e66d6d60dbaaf67edacd910d3dc
hoangminhphuc <22028059@vnu.edu.vn>
2025-04-25 16:23:27 +0700

    feat(base_model): add Mask method to generate encoded fake ID

--- common/models/base_model.go
+++ common/models/base_model.go
@@ -1,9 +1,26 @@
 package models
 
-import "time"
+import (
+	"net/http"
+	"time"
+
+	"github.com/hoangminhphuc/goph-chat/common"
+	"github.com/hoangminhphuc/goph-chat/common/utils"
+)
 
 type BaseModel struct {
 	ID        int        `json:"-" gorm:"column:id;"`
-	CreatedAt *time.Time `json:"-" gorm:"column:created_at;"`
-	UpdatedAt *time.Time `json:"-" gorm:"column:updated_at;"`
+	FakeID  	string  	`json:"id" gorm:"column:-;"`
+	CreatedAt *time.Time `json:"created_at" gorm:"column:created_at;"`
+	UpdatedAt *time.Time `json:"updated_at" gorm:"column:updated_at;"`
+}
+
+func (bModel *BaseModel) Mask() {
+	uid, err := utils.EncodeID(uint64(bModel.ID))
+
+	if err != nil {
+		common.WrapError(err, "failed to encode ID", http.StatusInternalServerError)
+	}
+
+	bModel.FakeID = uid
 }
