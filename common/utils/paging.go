b8f14acfb3c09e730385d1e5f2b16e8a2aa04eea
hoangminhphuc <22028059@vnu.edu.vn>
2025-04-27 11:13:03 +0700

    feat(utils): add Paging struct for pagination support

---
+++ common/utils/paging.go
@@ -0,0 +1,25 @@
+package utils
+
+
+type Paging struct {
+	Page  int `json:"page"`
+	Limit int `json:"limit"`
+	Total int64 `json:"total"`
+	FakeCursor string `json:"cursor" form:"cursor"`
+	NextCursor string `json:"next_cursor"`
+}
+
+
+func (p *Paging) Process() {
+	if p.Page <= 0 {
+		p.Page = 1
+	}
+
+	if p.Limit <= 0 {
+		p.Limit = 5
+	}
+
+	if p.Limit > 50 {
+		p.Limit = 50
+	}
+}
