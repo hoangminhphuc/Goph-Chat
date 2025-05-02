a6a20301db3fde84ef4dc31299b39a99e60e784e
hoangminhphuc <22028059@vnu.edu.vn>
2025-05-02 17:04:41 +0700

    feat(serviceHub): register redis service

--- cmd/root.go
+++ cmd/root.go
@@ -4,15 +4,17 @@ import (
 	"github.com/hoangminhphuc/goph-chat/boot"
 	"github.com/hoangminhphuc/goph-chat/common"
 	"github.com/hoangminhphuc/goph-chat/internal/router/register"
-	"github.com/hoangminhphuc/goph-chat/plugin/db"
+	"github.com/hoangminhphuc/goph-chat/plugin/gorm"
 	rt "github.com/hoangminhphuc/goph-chat/internal/router"
 	"github.com/hoangminhphuc/goph-chat/internal/server/websocket"
+	"github.com/hoangminhphuc/goph-chat/plugin/redis"
 )
 
 func newService() boot.ServiceHub {
 	service := boot.NewServiceHub(
 		"Goph-Chat",
 		boot.RegisterInitService(db.NewGormDB(common.PluginDBMain)),
+		boot.RegisterInitService(redis.NewRedisDB()),
 		boot.RegisterRuntimeService(rt.NewHTTPServer()),
     boot.RegisterRuntimeService(websocket.NewWebSocketServer()),
 	)
@@ -28,6 +30,7 @@ func Execute() {
 		logger.Log.Error(err.Error())
 	}
 
+	
 	serviceHub.InitializePools(serviceHub.MustGetRuntimeService(common.PluginWSMain).(*websocket.WebSocketServer))
 	register.RegisterAllRoutes(serviceHub.MustGetRuntimeService(common.PluginHTTPMain).(*rt.HTTPServer).GetRouter().Group("/"), serviceHub)
 
