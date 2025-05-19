d2591813b17867b93545829e6f02d5c9878a29c7
hoangminhphuc <22028059@vnu.edu.vn>
2025-05-19 23:56:56 +0700

    feat(queue): add Redis hash and key increment logic

--- internal/cache/queue.go
+++ internal/cache/queue.go
@@ -1,43 +1,66 @@
 package cache
 
-import (
-	"context"
-	"fmt"
-	"time"
 
-	"github.com/hoangminhphuc/goph-chat/module/message/model"
-	"github.com/redis/go-redis/v9"
+import (
+  "context"
+  "fmt"
+  "time"
+
+
+  "github.com/hoangminhphuc/goph-chat/module/message/model"
+  "github.com/redis/go-redis/v9"
 )
+
 
 const (
-	recentCacheSize = 100
-	recentTTL       = 24 * time.Hour
+  recentCacheSize = 100
+  recentTTL       = 24 * time.Hour
 )
 
+
 type MessageSaver interface {
-	SaveMessage(ctx context.Context, message *model.Message) error
+  SaveMessage(ctx context.Context, message *model.Message) error
 }
+
 
 type MessageQueue struct {
-	rdb *redis.Client
+  rdb *redis.Client
 }
 
+
 func NewMessageQueue(rdb *redis.Client) *MessageQueue {
-	return &MessageQueue{rdb: rdb}
+  return &MessageQueue{rdb: rdb}
 }
+
 
 // CacheAndQueue stores message to recent list and async queue
 func (mq *MessageQueue) CacheAndQueue(ctx context.Context, room string, userId string, payload []byte) error {
+  recentKey := fmt.Sprintf("messages:room:%s:user:%s:recent", room, userId)
+  queueKey  := fmt.Sprintf("queues:room:%s:user:%s", room, userId)
 
-	recentKey := fmt.Sprintf("messages:room:%s:user:%s:recent", room, userId)
-	queueKey  := fmt.Sprintf("queues:room:%s:user:%s", room, userId)
+  msgID, err := mq.rdb.Get(ctx, "next_message_id").Result()
+  if err != nil {
+      return err
+  }
+  msgKey := fmt.Sprintf("message:%s", msgID)
 
-	pipe := mq.rdb.Pipeline() 
-	pipe.LPush(ctx, recentKey, payload) // push on head of the list
-	pipe.LTrim(ctx, recentKey, 0, recentCacheSize-1) // cap at recentCacheSize
-	pipe.Expire(ctx, recentKey, recentTTL) // expire after TTL
-	pipe.RPush(ctx, queueKey, payload)
+  pipe := mq.rdb.Pipeline()
+  pipe.Incr(ctx, "next_message_id") // increment message ID
+  pipe.LPush(ctx, recentKey, msgID) // push on head of the list
+  pipe.LTrim(ctx, recentKey, 0, recentCacheSize-1) // cap at recentCacheSize
+  pipe.Expire(ctx, recentKey, recentTTL)
 
-	_, err := pipe.Exec(ctx)
-	return err
+  pipe.HSet(ctx, msgKey, map[string]interface{}{
+    "payload": payload, // marshalled message
+
+    // "created": msg.CreatedAt.UnixNano(),
+    // "edited":  msg.EditedAt.UnixNano(),
+  })
+  pipe.Expire(ctx, msgKey, recentTTL)
+
+  pipe.RPush(ctx, queueKey, msgID) // push to tail
+
+  _, err = pipe.Exec(ctx)
+
+  return err
 }
