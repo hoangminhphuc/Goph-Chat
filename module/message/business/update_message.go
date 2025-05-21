a2d69ce31b54ccebea7ea14b3ea215a2ae945844
hoangminhphuc <22028059@vnu.edu.vn>
2025-05-21 08:43:31 +0700

    feat(message): return message record after updating

--- module/message/business/update_message.go
+++ module/message/business/update_message.go
@@ -3,18 +3,18 @@ package business
 import (
 	"context"
 
-	"github.com/hoangminhphuc/goph-chat/internal/server/websocket"
 	"github.com/hoangminhphuc/goph-chat/module/message/dto"
+	"github.com/hoangminhphuc/goph-chat/module/message/model"
 )
 
 type UpdateMessageSQLRepo interface {
-	GetMessageByID(ctx context.Context, msgID int) (*websocket.Message, error)
+	GetMessageByID(ctx context.Context, msgID int) (*model.Message, error)
 	UpdateMessageByID(ctx context.Context, msgID int, data *dto.MessageUpdate) error
 }
 
 type UpdateMessageRedisRepo interface {
 	GetMessageInRecent(ctx context.Context, roomID, userID, msgID int) (bool, error)
-	UpdateHashMessage(ctx context.Context, msgID int, msgData *dto.MessageUpdate) error
+	UpdateHashMessage(ctx context.Context, msgID int, msgData *dto.MessageUpdate) (*model.Message, error)
 }
 
 type UpdateMessageBusiness struct {
@@ -31,41 +31,46 @@ func NewUpdateMessageBusiness(sqlRepo UpdateMessageSQLRepo,
 }
 
 func (b *UpdateMessageBusiness) UpdateMessageByID(
-    ctx context.Context, msgID int, data *dto.MessageUpdate) error {
+    ctx context.Context, msgID int, data *dto.MessageUpdate) (*model.Message, error) {
 			
     // Check if message is in recent list
     recent, err := b.redisRepo.GetMessageInRecent(ctx, data.RoomID, data.UserID, msgID)
     if err != nil {
-        return err
+        return nil, err
     }
 
-    // Check if message is in recent list
+    // Check if message is in DB
     record, err := b.sqlRepo.GetMessageByID(ctx, msgID)
     if err != nil {
-        return err
+        return nil, err
     }
 
-		// If record not found, update only in Redis
+	// If record not found, update only in Redis
     if record == nil {
-        if recent {
+        if recent { // Return message in Redis not in DB
             return b.redisRepo.UpdateHashMessage(ctx, msgID, data)
         }
-        return nil
+        return nil, nil
     }
 
     // If record found, update in DB
     if err := b.sqlRepo.UpdateMessageByID(ctx, msgID, data); err != nil {
-        return err
+        return nil, err
+    }
+
+    updated, err := b.sqlRepo.GetMessageByID(ctx, msgID)
+    if err != nil {
+        return nil, err
     }
 
     // Then update in Redis
     if recent {
-        if err := b.redisRepo.UpdateHashMessage(ctx, msgID, data); err != nil {
-            return err
+        if _, err := b.redisRepo.UpdateHashMessage(ctx, msgID, data); err != nil {
+            return nil, err
         }
     }
-
-    return nil
+    // Return message in DB
+    return updated, nil
 }
 
 
