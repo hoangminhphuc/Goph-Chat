c8743210d5abf26a3dec855686d997a2c123ce5c
hoangminhphuc <22028059@vnu.edu.vn>
2025-05-24 15:54:08 +0700

    fix(room): debug pointer references local pubsub

--- module/room/transport/rest/create_room_handler.go
+++ module/room/transport/rest/create_room_handler.go
@@ -20,7 +20,7 @@ func CreateRoom(serviceCtx serviceHub.ServiceHub) func(*gin.Context) {
 	return func(c *gin.Context) {
 		db := serviceCtx.MustGetService(common.PluginDBMain).(*gorm.DB)
 		wsServer := serviceCtx.MustGetRuntimeService(common.PluginWSMain).(*ws.WebSocketServer)
-		pubsub := serviceCtx.MustGetService(common.PluginPubSubMain).(pubsub.LocalPubSub)
+		pubsub := serviceCtx.MustGetService(common.PluginPubSubMain).(*pubsub.LocalPubSub)
 
 		var data dto.RoomCreation
 		if err := c.ShouldBind(&data); err != nil {
@@ -43,7 +43,7 @@ func CreateRoom(serviceCtx serviceHub.ServiceHub) func(*gin.Context) {
 		! THIS SHOULD NOT BE IN HERE, WILL BE IMPROVE LATER
 		! BY INTEGRATING EVENT-DRIVEN ARCHITECTURE (PUB/SUB)
 		*/
-		if err := wsServer.CreateRoom(data.ID, &pubsub); err != nil {
+		if err := wsServer.CreateRoom(data.ID, pubsub); err != nil {
 			common.ErrorResponse(c, http.StatusBadRequest, err.Error())
 			return
 		}
